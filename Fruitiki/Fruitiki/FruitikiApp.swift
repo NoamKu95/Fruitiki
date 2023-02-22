51bce0a6536bee75441285e89b98396247740003
Noam Kurtzer <noamkurtzer@gmail.com>
2023-02-22 10:28:52 +0200

    Project init and resources

---
+++ Fruitiki/Fruitiki/FruitikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitikiApp.swift
+//  Fruitiki
+//
+//  Created by Noam Kurtzer on 22/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
